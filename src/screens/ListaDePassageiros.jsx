06f16d0496ca82111e95e185cc662177bd9aa7b6
Ernane Barreto <juniobarret@gmail.com>
2023-09-24 10:16:02 -0300

    Exibição melhor das faculdades

--- src/screens/ListaDePassageiros.jsx
+++ src/screens/ListaDePassageiros.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import { View, Text, FlatList, StyleSheet, Picker } from "react-native";
 import { collection, query, where, getDocs, doc } from "firebase/firestore";
 import { db } from "../conf/firebase";
 import ButtonRetorno from "../components/Button-retorno";
@@ -7,11 +7,12 @@ import ButtonRetorno from "../components/Button-retorno";
 function ListaDePassageiros() {
   const [passageiros, setPassageiros] = useState([]);
   const [filtroData, setFiltroData] = useState("");
+  const [filtroFaculdade, setFiltroFaculdade] = useState("Todas");
 
   useEffect(() => {
     const fetchPassageiros = async () => {
       try {
-        const viagemRef = doc(db, "viagens", filtroData); // Referência para o documento de viagem correto
+        const viagemRef = doc(db, "viagens", filtroData);
         const q = query(collection(viagemRef, "passageiros"));
 
         const querySnapshot = await getDocs(q);
@@ -32,6 +33,44 @@ function ListaDePassageiros() {
     }
   }, [filtroData]);
 
+  const faculdadesCores = {
+    IFNMG: "#195128",
+    UNOPAR: "#0d1c4f",
+    ALFA: "#a3550b",
+    UNIPAC: "#d1c702",
+    DOCTUM: "#414141",
+    UNIMONTES: "#029dd1",
+  };
+
+  const filtrarPassageiros = () => {
+    if (filtroFaculdade === "Todas") {
+      return passageiros;
+    }
+    return passageiros.filter((passageiro) => passageiro.faculdade === filtroFaculdade);
+  };
+
+  const passageirosFiltrados = filtrarPassageiros();
+
+  const agruparPorFaculdade = () => {
+    const passageirosAgrupados = {};
+
+    passageirosFiltrados.forEach((passageiro) => {
+      if (!passageirosAgrupados[passageiro.faculdade]) {
+        passageirosAgrupados[passageiro.faculdade] = [];
+      }
+
+      passageirosAgrupados[passageiro.faculdade].push(passageiro);
+    });
+
+    return passageirosAgrupados;
+  };
+
+  const passageirosAgrupados = agruparPorFaculdade();
+
+  const faculdadesOrdenadas = Object.keys(passageirosAgrupados).sort();
+
+  const totalPassageiros = passageirosFiltrados.length;
+
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>Lista de Passageiros</Text>
@@ -44,25 +83,87 @@ function ListaDePassageiros() {
           onChange={(e) => setFiltroData(e.target.value)}
         />
       </View>
-      <FlatList
-        data={passageiros}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View style={styles.passageiroItem}>
-            <Text style={styles.label}>Nome:</Text>
-            <Text style={styles.dado}>{item.nome}</Text>
-            
-            <Text style={styles.label}>Faculdade:</Text>
-            <Text style={styles.dado}>{item.faculdade}</Text>
-            
-            <Text style={styles.label}>Data:</Text>
-            <Text style={styles.dado}>{item.dataCadastro.toDate().toLocaleDateString()}</Text>
-            
-            <Text style={styles.label}>Horário:</Text>
-            <Text style={styles.dado}>{item.dataCadastro.toDate().toLocaleTimeString()}</Text>
-          </View>
-        )}
-      />
+      <View style={styles.filtroContainer}>
+        <Text style={styles.filtroLabel}>Filtrar por Faculdade:</Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={filtroFaculdade}
+          onValueChange={(itemValue) => setFiltroFaculdade(itemValue)}
+        >
+          <Picker.Item label="Todas" value="Todas" />
+          {faculdadesOrdenadas.map((faculdade) => (
+            <Picker.Item key={faculdade} label={faculdade} value={faculdade} />
+          ))}
+        </Picker>
+      </View>
+      {filtroFaculdade === "Todas" ? (
+        <FlatList
+          data={faculdadesOrdenadas}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <View style={styles.faculdadeContainer}>
+              <Text style={styles.faculdadeTitulo}>{item}</Text>
+              {passageirosAgrupados[item].map((passageiro) => (
+                <View
+                  key={passageiro.id}
+                  style={{
+                    ...styles.passageiroItem,
+                    backgroundColor: faculdadesCores[passageiro.faculdade],
+                  }}
+                >
+                  <Text style={styles.label}>Nome:</Text>
+                  <Text style={styles.dado}>{passageiro.nome}</Text>
+
+                  <Text style={styles.label}>Faculdade:</Text>
+                  <Text style={styles.dado}>{passageiro.faculdade}</Text>
+
+                  <Text style={styles.label}>Data:</Text>
+                  <Text style={styles.dado}>
+                    {passageiro.dataCadastro.toDate().toLocaleDateString()}
+                  </Text>
+
+                  <Text style={styles.label}>Horário:</Text>
+                  <Text style={styles.dado}>
+                    {passageiro.dataCadastro.toDate().toLocaleTimeString()}
+                  </Text>
+                </View>
+              ))}
+            </View>
+          )}
+        />
+      ) : (
+        <FlatList
+          data={passageirosAgrupados[filtroFaculdade]}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View
+              style={{
+                ...styles.passageiroItem,
+                backgroundColor: faculdadesCores[item.faculdade],
+              }}
+            >
+              <Text style={styles.label}>Nome:</Text>
+              <Text style={styles.dado}>{item.nome}</Text>
+
+              <Text style={styles.label}>Faculdade:</Text>
+              <Text style={styles.dado}>{item.faculdade}</Text>
+
+              <Text style={styles.label}>Data:</Text>
+              <Text style={styles.dado}>
+                {item.dataCadastro.toDate().toLocaleDateString()}
+              </Text>
+
+              <Text style={styles.label}>Horário:</Text>
+              <Text style={styles.dado}>
+                {item.dataCadastro.toDate().toLocaleTimeString()}
+              </Text>
+            </View>
+          )}
+        />
+      )}
+      <Text style={styles.totalPassageiros}>
+        Total de Passageiros: {totalPassageiros}
+      </Text>
       <ButtonRetorno />
     </View>
   );
@@ -87,17 +188,18 @@ const styles = StyleSheet.create({
   filtroContainer: {
     flexDirection: "row",
     alignItems: "center",
-    marginBottom: 20,
+    marginBottom: 10,
   },
 
   filtroLabel: {
+    flex: 1,
     fontSize: 16,
     fontWeight: "bold",
-    marginRight: 10,
+    color: "#333",
   },
 
   filtroInput: {
-    flex: 1,
+    flex: 2,
     height: 40,
     backgroundColor: "#FFFFFF",
     borderRadius: 10,
@@ -106,24 +208,52 @@ const styles = StyleSheet.create({
     borderColor: "#1e7557",
   },
 
-  passageiroItem: {
+  picker: {
+    flex: 2,
+    height: 40,
     backgroundColor: "#FFFFFF",
     borderRadius: 10,
-    padding: 20,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#1e7557",
+  },
+
+  faculdadeContainer: {
     marginBottom: 20,
+  },
+
+  faculdadeTitulo: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+
+  passageiroItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
   },
 
   label: {
     fontSize: 16,
     fontWeight: "bold",
     color: "#333",
-    marginBottom: 5,
+    width: 80,
   },
 
   dado: {
     fontSize: 16,
-    color: "#555",
-    marginBottom: 15,
+    color: "#000",
+    flex: 1,
+  },
+
+  totalPassageiros: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 20,
   },
 });
 
